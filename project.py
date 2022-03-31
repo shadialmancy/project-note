76d4990cef5547a6e89ecb7fb4c268cf56787d36
shadialmancy <shadi.almancy.sa@gmail.com>
2022-03-31 17:40:23 +0200

    Update project.py

--- project.py
+++ project.py
@@ -2,24 +2,26 @@ print("----------Note----------")
 end = True
 dict={}
 while end:
-    x=input("Would you like to:\n1-Create\n2-Edit\n3-Delete\n4-Display")
+    x=input("Would you like to:\n1-Create\n2-Edit\n3-Delete\n4-Display\n")
     
     x=x.lower()
 
     if x=="create":
         name=input("enter the note name:")
         if name in dict:
+         print("name already used")
+        else:
          note=input("enter the note:")
          dict[name]=note
-        else:
-                        print("name already used")
+                        
     elif x=="edit":
         name=input("enter the note name:")
         if name in dict:
-         note=input("enter the new note")
-         dict[name]=note
+         print("note name not found")   
+         
         else:
-                        print("note name not found")
+         note=input("enter the new note")
+         dict[name]=note               
     elif x=="delete":
                     name=input("enter the note name:")
                     if name in dict:
@@ -32,9 +34,10 @@ while end:
             print(dict)
     else:
         print("Error: Input Undefined")
-    end=input("Would you like to contuine?\nTrue/False")
-    if end == True:
+    finish=input("Would you like to contuine?\nTrue/False").capitalize()
+    
+    if finish == "True":
                     continue
     else:
-                    break
+                    end=False
                 
